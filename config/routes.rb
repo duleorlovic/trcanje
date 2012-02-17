8845706c259de0c6da418e46bf011c0763a5d9e5
Dusan Orlovic <orlovics@eunet.rs>
2012-02-17 18:36:56 +0100

    finish layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Trcanje::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new"
+  match '/signin',to:"users#new"
+  match '/signup',to:"users#new"
+  match '/help',to: "static_pages#help"
+  match '/contact',to: 'static_pages#contact'
+  match '/about',to:'static_pages#about'
 
-  get "static_pages/help"
-
-  get 'static_pages/contact'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -53,7 +55,7 @@ Trcanje::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,23 +1,59 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      #get static_pages_index_path
-      #response.status.should be(200)
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
+  describe "Home page" do
+
+    it "should have the h1 'Sample App'" do
+      visit root_path
+      page.should have_selector('h1', text: 'Sample App')
     end
 
-    it "should have help page" do 
-      visit '/static_pages/help'
-      page.should have_selector('h1',:text=>'Help')
+    it "should have the title 'Home'" do
+      visit root_path
+      page.should have_selector('title',
+                        text: "Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit help_path
+      page.should have_selector('h1', text: 'Help')
     end
 
-    it 'should have the right title' do
-      visit '/static_pages/home'
-      page.should have_selector('title',:text=> "Home")
+    it "should have the title 'Help'" do
+      visit help_path
+      page.should have_selector('title',
+                        text: "Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About'" do
+      visit about_path
+      page.should have_selector('h1', text: 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit about_path
+      page.should have_selector('title',
+                    text: "About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title',
+                    text: "Contact")
     end
   end
 end
