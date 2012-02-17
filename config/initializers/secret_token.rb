35978819a8f4ce4b6336ddef0480be2f38d00b25
Dusan Orlovic <orlovics@eunet.rs>
2012-02-17 01:41:22 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trcanje::Application.config.secret_token = '5e82023c27921c8d0c563fbccd7136461cc1eafa21e14b6b3b92a720b31e1c59b4efb5cac9510cbf9e35227fc1b9b2075e67136a72f032a31f89d5588fe1f2f1'
